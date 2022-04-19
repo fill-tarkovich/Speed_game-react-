cc9760abd077acce86d3190165f3c8f831c4632e
Filipp Tarkovich <fill.tarkovich@gmail.com>
2022-04-19 14:49:01 +0300

    Updates

---
+++ precache-manifest.69667b4c6e20778fb10fbc4d7a6a2c74.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd3e8f972fa3851181b1",
+    "url": "/Speed_game-react-/static/css/main.692542bc.chunk.css"
+  },
+  {
+    "revision": "dd3e8f972fa3851181b1",
+    "url": "/Speed_game-react-/static/js/main.dd3e8f97.chunk.js"
+  },
+  {
+    "revision": "bedbc09506c2461e45c9",
+    "url": "/Speed_game-react-/static/js/1.bedbc095.chunk.js"
+  },
+  {
+    "revision": "8dee773443139cbed2ef",
+    "url": "/Speed_game-react-/static/js/runtime~main.8dee7734.js"
+  },
+  {
+    "revision": "3ee1573f5e549a0fdcf2a21c18259a7f",
+    "url": "/Speed_game-react-/static/media/bg.3ee1573f.mp3"
+  },
+  {
+    "revision": "55f9d8458f8f66666c239fb3e0d36ffc",
+    "url": "/Speed_game-react-/static/media/end.55f9d845.mp3"
+  },
+  {
+    "revision": "a89eaf1433682474746640ad7d5c09b1",
+    "url": "/Speed_game-react-/static/media/1329309424.a89eaf14.png"
+  },
+  {
+    "revision": "ff428d26d07dfce5ff536c52bc37b1b0",
+    "url": "/Speed_game-react-/index.html"
+  }
+];
